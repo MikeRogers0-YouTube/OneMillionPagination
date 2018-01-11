e8feb27c4d415010f160940bce87a4d6c4f47f27
Mike Rogers <me@mikerogers.io>
2018-01-11 16:28:14 +0000

    Outputting numbers

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,4 +1,9 @@
 class NumbersController < ApplicationController
   def index
   end
+
+  helper_method :resources
+  def resources
+    @resources ||= Paginators::NumbersPaginator.new(per_page: params[:per_page], page: params[:page])
+  end
 end
--- app/lib/paginators/numbers_paginator.rb
+++ app/lib/paginators/numbers_paginator.rb
@@ -1,6 +1,30 @@
 class Paginators::NumbersPaginator
-  def initialize(per_page: nil, page: nil)
-    @per_page = per_page || 1000
-    @page = page || 1
+  include Enumerable
+  extend Forwardable
+
+  # Calling .each on this object, will call it on the private store method.
+  def_delegators :store, :each, :first, :last, :size
+
+  def initialize(per_page: 1000, page: 1)
+
+    # Validate both elements can be positive integers, otherwise reset them to default
+    per_page = 1000 if per_page.to_i <= 0
+    page = 1 if page.to_i <= 0
+
+    @per_page = per_page.to_i
+    @page = page.to_i
+  end
+
+  private
+  def store
+    (starting_number..ending_number)
+  end
+
+  def starting_number
+    ((@page - 1) * @per_page) + 1
+  end
+
+  def ending_number
+    (@page * @per_page)
   end
 end

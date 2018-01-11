20179ad555091e20c8d5cee0b7a8dbb3014001b5
Mike Rogers <me@mikerogers.io>
2018-01-11 18:06:38 +0000

    Reread spec - joining with 'and' is fine

--- test/lib/integer_test.rb
+++ test/lib/integer_test.rb
@@ -11,9 +11,9 @@ class IntegerTest < ActiveSupport::TestCase
     assert_equal 'eight hundred and fifty-one', 852.to_words
     assert_equal 'one thousand and fifty', 1050.to_words
     assert_equal 'one thousand and fifty', 1050.to_words
-    assert_equal 'four hundred and ninety thousand, one hundred and twenty', 490_120.to_words
+    assert_equal 'four hundred and ninety thousand and one hundred and twenty', 490_120.to_words
     assert_equal 'five hundred thousand', 500_000.to_words
-    assert_equal 'nine hundred and ninety-nine thousand, nine hundred and ninety-nine', 999_999.to_words
+    assert_equal 'nine hundred and ninety-nine thousand and nine hundred and ninety-nine', 999_999.to_words
     assert_equal 'one million', 1_000_000.to_words
   end
 end

f084b765d47ef442de7059a39b9a159c676863cf
Mike Rogers <me@mikerogers.io>
2018-01-11 18:13:47 +0000

    Tidying up code - Will need to document a bit better so I can explain tomorrow

--- config/initializers/integer.rb
+++ config/initializers/integer.rb
@@ -29,9 +29,8 @@ class Integer
       5 => 'five',
       4 => 'four',
       3 => 'three',
-      2 => 'teo',
-      1 => 'one',
-      0 => 'zero'
+      2 => 'two',
+      1 => 'one'
     }
 
     word_phrase = ''
@@ -40,10 +39,25 @@ class Integer
     # block of values.
     
     steps_as_words.each do |num, word|
-      return word_phrase if self == 0
-      return "#{word_phrase} #{word}" if self < 9 && self/num > 0
+      # The number is 0 - return blank
+      return '' if self == 0
+
+      # The number is between 1 & 9
+      return "#{word_phrase}#{word}" if self < 9 && self/num > 0
+
+      # The number is between 10 & 100
+      if self < 100 && (self / num) > 0
+        # Number has no smaller version of it (so 20, not 25)
+        return "#{word_phrase}#{word}" if self % num == 0
+        return "#{word_phrase}#{word}-#{(self % num).to_words}"
+      end
+
+      if (self / num) > 0
+        return "#{word_phrase}#{(self / num).to_words} #{word} #{'and' if self % num != 0} #{(self % num).to_words}".strip
+      end
     end
     
-    self.strip
+    # Some unknown number, just return itself.
+    self
   end
 end
--- test/lib/integer_test.rb
+++ test/lib/integer_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class IntegerTest < ActiveSupport::TestCase
-  # I've monkey patched the Integer class to add the `to_words` method.
+  # I've monkey patched the Integer class to add the `to_words` method.
   test '.to_words' do
     assert_equal 'one', 1.to_words
     assert_equal 'five', 5.to_words
     assert_equal 'eleven', 11.to_words
     assert_equal 'twenty', 20.to_words
     assert_equal 'twenty-six', 26.to_words
-    assert_equal 'eight hundred and fifty-one', 852.to_words
+    assert_equal 'eight hundred and fifty-two', 852.to_words
     assert_equal 'one thousand and fifty', 1050.to_words
     assert_equal 'one thousand and fifty', 1050.to_words
     assert_equal 'four hundred and ninety thousand and one hundred and twenty', 490_120.to_words
